b0cbd9c26347bdd303acf70dbf7e81adcf390d08
IPJ <cluj1ipa@gmail.com>
2023-08-29 09:18:18 +0300

    filter update

--- app/(user)/(index)/page.tsx
+++ app/(user)/(index)/page.tsx
@@ -7,32 +7,25 @@ import axios from "axios";
 
 interface RootPageProps {
   searchParams: {
-    id: string ;
-    name:string ;
+    id: string;
+    name: string;
   };
 }
 
-// export const revalidate = 10;
 const Home = ({ searchParams }: RootPageProps) => {
-  const [categories, setCategories] = useState<any[]>([]); // Use the appropriate type for your data
-  const [cards, setCards] = useState<any[]>([]); // Use the appropriate type for your data
+  const [categories, setCategories] = useState<any[]>([]);
+  const [cards, setCards] = useState<any[]>([]);
+  const [filteredCards, setFilteredCards] = useState<any[]>([]); // Store the filtered cards
 
-  const fetchData = async (id: string) => {
+  const fetchData = async () => {
     try {
-    
-      const queryString = new URLSearchParams(searchParams).toString()
-      console.log(queryString);
-
-      console.log(searchParams.id, searchParams.name)
-      const response = await fetch(`/api/oferte/${queryString || 'all'}`, {
+      // const queryString = new URLSearchParams(searchParams).toString()
+      const response = await fetch(`/api/oferte/}`, {
         cache: "no-cache",
       }); //
-      // console.log(response)
       const responseData = await response.json();
       const categories = responseData.categories;
       const cards = responseData.cards;
-      
-      
       setCategories(categories);
       setCards(cards);
     } catch (error) {
@@ -41,8 +34,44 @@ const Home = ({ searchParams }: RootPageProps) => {
   };
 
   useEffect(() => {
-    fetchData(searchParams.id);
-  }, [searchParams.id, searchParams.name]); // Re-fetch data whenever the categoryId changes
+    fetchData();
+  }, []); // Re-fetch data whenever the categoryId changes
+
+  const sortData = (searchParams: RootPageProps["searchParams"]) => {
+    console.log(searchParams.id, searchParams.name);
+    const selectedCategory = categories.find(
+      (category) => category.name === searchParams.id
+    );
+    if (searchParams.id == "Toate") {
+      console.log(cards);
+      setFilteredCards(cards);
+    } else if (selectedCategory) {
+      // Filter cards based on the selected category's id
+      const cardsInSelectedCategory = cards.filter(
+        (card) => card.categoryId === selectedCategory.id
+      );
+      if (cardsInSelectedCategory) {
+        setFilteredCards(cardsInSelectedCategory);
+      } else {
+        setFilteredCards(cards);
+      }
+    }
+    if(searchParams.name){
+      const fcards = cards.filter(card =>
+        card.name.toLowerCase().includes(searchParams.name.toLowerCase()) // 
+      );
+      setFilteredCards(fcards)
+    }
+
+    // Sorting logic here to create 'sortedData'
+    // const sortedData = searchParams.sort(/* sorting logic */);
+
+    return 0;
+  };
+
+  useEffect(() => {
+    sortData(searchParams);
+  }, [searchParams.id, searchParams.name, searchParams]); // Re-fetch data whenever the categoryId changes
 
   return (
     <div className="h-full  mr-2 ">
@@ -50,9 +79,10 @@ const Home = ({ searchParams }: RootPageProps) => {
         <CatMenu categories={categories} />
       </div>
       <div className="">
-        <Card data={cards}></Card>
+        <Card data={filteredCards}></Card>
       </div>
     </div>
   );
 };
 export default Home;
+export const dynamic = "force-dynamic";
--- app/api/oferte/[categoryId]/route.ts
+++ app/api/oferte/[categoryId]/route.ts
@@ -1,52 +1,10 @@
 import prismadb from "@/lib/prismadb";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
-  req: NextRequest,
-  // res: Response,
-  {
-    params,
-  }: {
-    params: {
-      categoryId?: any;
-    };
-  }
-) {
+export async function GET(req: NextRequest) {
   try {
-    const getparams = new URLSearchParams(params.categoryId);
-
-    const id = getparams.get("id") as string;
-    const name = getparams.get("name") as string;
-    console.log(name);
+    var cards = await prismadb.card.findMany();
     const categories = await prismadb.category.findMany();
-
-    if (id == null || id == "Toate" || id == undefined) {
-      var cards = await prismadb.card.findMany();
-      cards = await prismadb.card.findMany({
-        where: {
-          name: {
-            contains: name || "",
-          },
-        },
-      });
-    } else {
-      const filtedcat = await prismadb.category.findMany({
-        where: {
-          name: {
-            equals: id,
-          },
-        },
-      });
-
-      cards = await prismadb.card.findMany({
-        where: {
-          categoryId: filtedcat[0]?.id,
-          name: {
-            contains: name || "",
-          },
-        },
-      });
-    }
 
     return NextResponse.json({ categories, cards });
   } catch (error) {
